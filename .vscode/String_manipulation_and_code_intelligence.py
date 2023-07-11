c22c34aa023d34e7d7d82fa796bbb01ac670b6b0
Prathamesh Chavan <prathamchavan0013@gmail.com>
2023-07-11 16:23:15 +0530

    input function try

--- .vscode/String_manipulation_and_code_intelligence.py
+++ .vscode/String_manipulation_and_code_intelligence.py
@@ -2,3 +2,8 @@ print("hello world!\nhello world!")
 
 #string concatination
 print("hello" + " Pratham")
+
+print("Day 1 - String manipulation")
+print("String Concentration is done with the '+' sign.")
+print('\n\t e.g. print("Hello" + "world")')
+print("New lines can be created using backslash and n")
