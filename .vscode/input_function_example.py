3b1c39e380e16a1efef2441e867577b9d6ba8cbb
Prathamesh Chavan <prathamchavan0013@gmail.com>
2023-07-11 16:34:48 +0530

    input function small problem

---
+++ .vscode/input_function_example.py
@@ -0,0 +1,3 @@
+#program tha prints the number of characters in user's name
+
+print(len(input("What is your name ?")))
--- .vscode/input_function_try.py
+++ .vscode/input_function_try.py
@@ -1,1 +1,4 @@
+#input() will get user input in console
+#then print() will print the word "hello" and the user input
+
 print("hello" +" "+input(" what is your name? "))
