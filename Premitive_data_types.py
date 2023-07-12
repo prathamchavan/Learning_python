61c557c4f02d5ed5751589179283101891a11581
Prathamesh Chavan <prathamchavan0013@gmail.com>
2023-07-12 19:22:05 +0530

    premitive data types

---
+++ Premitive_data_types.py
@@ -0,0 +1,28 @@
+ # data types
+
+#string 
+
+print("hello"[0])   #so this method of pulling out perticular element
+print("hello"[1])   #from the string is called as Subscript
+print("hello"[2])
+print("hello"[3])
+print("hello"[4])
+
+#no mathematical operation is performed as both considered as string
+print("123"+"345")
+
+#Integer datatype
+
+print(123+345) #here numbers are considerd as Intiger datatype
+
+
+#Floating point number 
+print(1.2 + 1.2) 
+
+
+#Boolean
+#basically boolean datatype returns the true or false value
+
+print(bool(1==1))
+
+print(bool(7<6))
