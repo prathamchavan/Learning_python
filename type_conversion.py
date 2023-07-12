07c000af01481226020cc367a6d4269d76af5d35
Prathamesh Chavan <prathamchavan0013@gmail.com>
2023-07-12 19:44:36 +0530

    type fuction and type conversion

---
+++ type_conversion.py
@@ -0,0 +1,11 @@
+num_char = len(input("What is your name?"))
+
+#here we are have converted intiger value into string
+new_num_char = str(num_char)
+
+print("Your name has "+new_num_char+" characters.")
+
+print(type(new_num_char))
+
+
+#type() function is used to investigate data type ypu are working with
