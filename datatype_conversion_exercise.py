ea687c42bffa9d40368c9508fee74986a03e7bd0
Prathamesh Chavan <prathamchavan0013@gmail.com>
2023-07-12 22:52:40 +0530

    datatype conversion exrecise

---
+++ datatype_conversion_exercise.py
@@ -0,0 +1,13 @@
+# here we have to input two digit number and 
+# calculate sum of the digit
+
+two_digit_number = input("Enter two digit number: ")
+
+first = two_digit_number[0]
+second = two_digit_number[1]
+
+print(first)
+print(second)
+
+result = int(first) + int(second)
+print(result)
