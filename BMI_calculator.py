310ad76add4436feec63e5bfd8a7a6c9fadc5446
Prathamesh Chavan <prathamchavan0013@gmail.com>
2023-07-12 23:47:20 +0530

    BMI calculator

---
+++ BMI_calculator.py
@@ -0,0 +1,10 @@
+weight = input("Please input your weight in kilogram: ")
+print((weight))
+height = input("Please input your height in meters: ")
+print((height))
+
+#using an exponent operator
+BMI = int(weight) / float(height)**2
+
+#type-conversion
+print(int(BMI))
