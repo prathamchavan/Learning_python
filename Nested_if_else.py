441d4766a557ac1c0858abc7e1fa0cf37c01c39f
Prathamesh Chavan <prathamchavan0013@gmail.com>
2023-07-13 11:05:07 +0530

    nested if else

---
+++ Nested_if_else.py
@@ -0,0 +1,12 @@
+#example for nested if else 
+height = int(input("Enter your height in cm "))
+
+if height >= 120:
+    print("You can ride the rollercoaster.")
+    age = int(input("What is your age? "))
+    if age > 18:
+        print("You have to pay $12.")
+    else:
+        print("You have to pay $7.")
+else:
+    print("Sorry, you can not ride the rollercoaster")
